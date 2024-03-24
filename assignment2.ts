fbd0706c41a8468275e1fab17ec3d51089de0ee8
shortrology <163891592+shortrology@users.noreply.github.com>
2024-03-24 20:06:02 +0500

    Add files via upload

---
+++ assignment2.ts
@@ -0,0 +1,67 @@
+//objects => {} => { firstnameiskey: bilalisvalue}  pair 
+//example 1 {[]}
+
+let user = {
+    firstName: "bilal",
+    lastName : "anwar",
+    age : "25",
+    hobbies : ["criket","football","gaming"] 
+}
+console.log(user);
+
+//example 2 {[{}]}     /const is fix value 
+
+const  customer = {
+       name : "bilal anwar",
+       contact : 9245667888,
+       item : ["bags", "shoes","dresses","makeup",],
+       address :{
+        country : "pakistan",
+        city :"karachi",
+        area : "aiwan-e-sadar",
+       }
+
+}
+
+console.log(customer.name);
+
+//example 3 {{}}
+
+
+let seller = {
+     name : "umer",
+     emailaddress : "umer anwar gamil.com ",
+    itemNo : 1234,
+    password : true,
+    parcel :null,
+     address : {
+        country : "pakistan",
+        city :"karachi",
+     }
+     ,
+    }
+
+console.log( seller.address.country);
+
+// arrrays => [] =>      example 4 [{}]
+
+let taecherName =[
+    " bial answer", "idName",
+     {
+        workingDays : 30,
+        overTime : "2 hour",
+        offDays : 3,
+        totalSalary : 15000,
+        payable :14000, 
+    }
+
+]
+
+console.log(taecherName);
+
+
+
+
+
+
+
